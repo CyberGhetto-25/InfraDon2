ae81f6111724a72b87fe683aa4d61c89254890bf
CyberGhetto-25 <bridy.marc@gmail.com>
2025-10-09 11:58:30 +0200

    chore:import pouchdb on TheWelcome & start implement functions

--- InfraDon2 - noSQL/src/components/TheWelcome.vue
+++ InfraDon2 - noSQL/src/components/TheWelcome.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 
 import { ref } from 'vue'
+import PouchDB from 'pouchdb'
 //DATA VARIABLES
 const counter = ref(0);
-
+const db = new PouchDB('http://admin:admin@localhost:5984/test_infradon2');
 //FUNCTIONS / METHODS -> CONTROLLER
 const increment = () => {
   counter.value++
+}
+
+const initDatabase = () => {
+
+}
+
+const mounted = () => {
+    this.initDatabase()
+    this.fetchData()
 }
 
 </script>
